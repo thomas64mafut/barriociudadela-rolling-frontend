019210ab283ee8c036fff9ece551b0159fb84b28
thomas mafut <mafut50@gmail.com>
2023-01-05 21:00:48 -0300

    font and logo imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/fonts/Blogh.otf'
+import './assets/fonts/Leonardo-Rounded.otf'
+import './assets/fonts/VictorMono.ttf'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
