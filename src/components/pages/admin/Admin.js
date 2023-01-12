26570612b3388e82921ffe606b1f3714581e6c42
thomas mafut <mafut50@gmail.com>
2023-01-12 19:47:57 -0300

    added editRole from usersTable

--- src/components/pages/admin/Admin.js
+++ src/components/pages/admin/Admin.js
@@ -1,19 +1,23 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Table, Button, Alert } from "react-bootstrap";
 import "./admin.css";
+import React, { useEffect, useState } from "react";
+import { Table, Button, Alert, Dropdown } from "react-bootstrap";
+import axios from "axios";
+import UserPlus from '../../../assets/icons/light/UserPlus';
+import UserX from '../../../assets/icons/light/UserX';
 
 const Admin = () => {
     const [usersToShow, setUsersToShow] = useState([]);
-    const [errorMessage, setErrorMessage] = useState("");
+    const [rolesToShow, setRolesToShow] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
         handleGetUsers();
+        getRoles();
     }, []);
 
     const handleGetUsers = async () => {
         try {
-            const { data } = await axios.get("http://localhost:4000/api/user/");
+            const { data } = await axios.get('http://localhost:4000/api/user/');
             console.log(data);
             setUsersToShow(data?.users);
         } catch (error) {
@@ -25,7 +29,7 @@ const Admin = () => {
     const handleDeleteUser = async (id) => {
         try {
             const { data } = await axios.patch(
-                `http://localhost:4000/api/user/${id}`
+                `http://localhost:4000/api/user/delete/${id}`
             );
             console.log(data);
             handleGetUsers();
@@ -39,32 +43,68 @@ const Admin = () => {
         return formattedDate.toDateString();
     };
 
+    const getRoles = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:4000/api/role');
+            setRolesToShow(data.roles);
+        } catch (error) {
+            console.log('mori');
+        }
+    }
+    
+    const editRole = async (id, roleId) => {
+        try {
+            const { data } = await axios.patch(`http://localhost:4000/api/user/${id}`, { role: roleId });
+            console.log(data);
+        } catch (error) {
+            console.log('mori');
+        }
+    }
+
     return (
         <>
             <div className="abm-container">
                 <div className="users-header">
                     <h1>User Control Panel</h1>
-                    <Button onClick={handleGetUsers}>Add User</Button>
+                    <Button onClick={handleGetUsers} variant='success'>
+                        <UserPlus />
+                    </Button>
                 </div>
                 {errorMessage ? (
                     <Alert variant="danger">{errorMessage}</Alert>
                 ) : (
                     ""
                 )}
-                <Table className="table-container">
+                <Table className="table-container" size="sm">
                     <thead>
                         <tr>
                             <th>role</th>
                             <th>username</th>
                             <th>email</th>
                             <th>member since</th>
-                            <th>delete</th>
+                            <th>options</th>
                         </tr>
                     </thead>
                     <tbody>
                         {usersToShow?.map((user) => (
                             <tr>
-                                <td>{user.role?.name}</td>
+                                <td>
+                                    <Dropdown className="m-2">
+                                        <Dropdown.Toggle variant='danger' className="btn-dropdown">
+                                            {user.role?.name}
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            {
+                                                rolesToShow ? 
+                                                    rolesToShow.map((role) => (
+                                                        <Dropdown.Item onClick={() => editRole(user?._id, role?._id)}>{role?.name}</Dropdown.Item>
+                                                    ))
+                                                :
+                                                    ''
+                                            }
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </td>
                                 <td>{user.username}</td>
                                 <td>{user.email}</td>
                                 <td>{dateFormatter(user.createdAt)}</td>
@@ -73,8 +113,10 @@ const Admin = () => {
                                         onClick={() =>
                                             handleDeleteUser(user._id)
                                         }
+                                        variant='danger'
+                                        className="btn-User"
                                     >
-                                        x
+                                        <UserX/> 
                                     </Button>
                                 </td>
                             </tr>
