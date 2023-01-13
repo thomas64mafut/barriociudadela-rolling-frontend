e00bf37270133b327ffbb32d6178429a643dfcb6
Tomas Garcia Cardenas <tomass11052001@hotmail.com>
2023-01-13 02:47:09 -0300

    nuevo navbar prueba

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,12 @@
 import React from 'react'
 import './header.css'
 import logo from '../../assets/img/logo-red.png'
+import Navbar from './Nav'
 
 const Header = () => {
   return (
     <div>
-      <div className='navHome d-flex justify-content-around align-items-center'>
-        <div>
-          <a className='navOptions boton1'>ABOUT US</a>
-          <a className='navOptions boton1'>MENUS</a>
-          <a className='navOptions boton1'>FOLLOW US</a>
-        </div>
-        <div>
-          <img className='logo' src={logo} alt=''/>
-        </div>
-        <div>
-          <a className='navOptions boton1'>CONTACT US</a>
-          <a className='navOptions boton1'>LOGIN</a>
-          <a className='navOptions boton1'>REGISTER</a>
-        </div>
-      </div>
+      <Navbar/>
     </div>
   )
 }
