b34c10675ec21d5c4cf617dd8d1a8c1de666d098
Tomas Garcia Cardenas <tomass11052001@hotmail.com>
2023-01-13 01:17:22 -0300

    fotos de hamburguesas en img y carousel del home

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './home.css'
+import Carousel from './Carousel'
 
 const Home = () => {
   return (
-    <div className='homeContainer'>Home</div>
+    <div className='homeContainer'>
+      <Carousel/>
+      <div className='welcomeContainer'>
+        <div className='text-white welcome'>WELCOME</div>
+        <div className='text-white under-welcome'>To Barrio Ciudadela, our home in Tucuman.</div>
+      </div>
+    </div>
   )
 }
 
