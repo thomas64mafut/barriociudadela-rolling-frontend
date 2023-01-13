841b421e2c7713410e94366c2f248f4e14b3b4b8
Tomas Garcia Cardenas <tomass11052001@hotmail.com>
2023-01-13 03:03:45 -0300

    nuevos cambios en navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,39 @@
-import React from 'react'
 import './header.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/img/logo-red.png'
-import Navbar from './Nav'
 
 const Header = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
+    <Navbar className='navContainer' bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#"></Navbar.Brand>
+        <img className='logoa' src={logo}/>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0 locura"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <div className='primerNav'>
+              <div href="#action1" className='navOptions boton1'>ABOUT US</div>
+              <div href="#action2" className='navOptions boton1'>MENU</div>
+              <div href="#" className='navOptions boton1' >
+                FOLLOW US
+              </div>
+            </div>
+          </Nav>
+          <div className='segundoNav'>
+            <div className='navOptions boton1'>CONTACT US</div>
+            <div className='navOptions boton1'>LOGIN</div>
+            <div className='navOptions boton1'>REGISTER</div>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
