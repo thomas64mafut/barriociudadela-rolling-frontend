61ec7253f50f515a07a984604b29dad1f588cf74
thomas mafut <mafut50@gmail.com>
2023-01-16 20:12:37 -0300

    eliminado de archivos commiteados por error

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,20 +4,20 @@ import logo from '../../assets/img/logo-red.png'
 
 const Header = () => {
   return (
-    <div className='headerContainer'>
+    <div>
       <div className='navHome d-flex justify-content-around align-items-center'>
         <div>
-          <a className='navOptions boton1'>about us</a>
-          <a className='navOptions boton1'>menu</a>
-          <a className='navOptions boton1'>follow us</a>
+          <a className='navOptions boton1'>ABOUT US</a>
+          <a className='navOptions boton1'>MENUS</a>
+          <a className='navOptions boton1'>FOLLOW US</a>
         </div>
         <div>
           <img className='logo' src={logo} alt=''/>
         </div>
         <div>
-          <a className='navOptions boton1'>contact us</a>
-          <a className='navOptions boton1'>login</a>
-          <a className='navOptions boton1'>register</a>
+          <a className='navOptions boton1'>CONTACT US</a>
+          <a className='navOptions boton1'>LOGIN</a>
+          <a className='navOptions boton1'>REGISTER</a>
         </div>
       </div>
     </div>
