8516ebbf8f0c66b7fdde23e3bc260a8bf73c9663
Mauricio Dorado Lenz <mauridlenz@gmail.com>
2023-01-18 17:31:36 -0300

    add rol verification

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,25 +1,41 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '../components/layout/Layout'
-import AboutUs from '../components/pages/about us/AboutUs'
 import Home from '../components/pages/home/Home'
 import Login from '../components/pages/login/Login'
 import Orders from '../components/pages/orders/Orders'
 import Register from '../components/pages/register/Register'
 import Admin from '../components/pages/admin/Admin';
+import RequireAuth from './../components/requireAuthorization/RequireAuth';
+import Unauthorized from '../components/pages/Unauthorized'
+import Missing from '../components/pages/Missing'
+
+/*const ROLES = {
+  'User': '63b750d9df706678b4bf105c',
+  'Admin': '63b36e00cf1222d30f57dcff',
+};*/
 
 const Router = () => {
   return (
     <BrowserRouter>
         <Layout>
-            <Routes>
+          <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
-                <Route path='/orders' element={<Orders/>}/>
-                <Route path='/aboutUs' element={<AboutUs/>}/>
-                <Route path='/admin' element={<Admin/>}/>
-            </Routes>
+                <Route path="unauthorized" element={<Unauthorized />} />
+
+                <Route element={<RequireAuth  />}>
+                  <Route path='/orders' element={<Orders/>}/>
+                </Route>
+
+                <Route element={<RequireAuth  />}>
+                  <Route path='/admin' element={<Admin/>}/>
+                </Route>
+                
+                {/* catch all */}
+                <Route path="*" element={<Missing />} />
+          </Routes>
         </Layout>
     </BrowserRouter>
   )
