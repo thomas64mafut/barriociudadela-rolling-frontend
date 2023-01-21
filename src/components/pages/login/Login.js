5c80950ff237e29923b51e3879dec20a7ac2e511
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2023-01-21 17:30:05 -0300

    add login/register w jwt  (#15)
    * add jwt validations
    
    * add rol verification
    
    * added login valdation to routes w useAuth (#13)
    
    * try fixing login & role validation
    
    * login & verify login fix
    
    * general indenting
    
    * fixed comment on login
    
    * deleted line
    
    * basic style added to login (#14)
    
    * added style to register, changed to bs form
    
    * added icons, fixed some style of info fields
    
    * login/register finished
    
    Co-authored-by: Mauricio Dorado Lenz <mauridlenz@gmail.com>

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,14 +1,12 @@
 import './login.css'
-import { useRef, useState, useEffect } from 'react';
-import useAuth from '../../../hooks/useAuth';
 import axios from '../../../api/axios';
+import { useRef, useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
 
 const LOGIN_URL = '/api/user/login';
 
 const Login = () => {
-    const { setAuth } = useAuth();
-
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || "/";
@@ -19,7 +17,7 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMsg, setErrorMsg] = useState('');
-    
+
     useEffect(() => {
         emailRef.current.focus();
     }, [])
@@ -30,16 +28,9 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         try {
-            const response = await axios.post(LOGIN_URL,
-                JSON.stringify({ email, password }),
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                    //withCredentials: true
-                }
-            );
-            setAuth({ email, password });
+            const response = await axios.post(LOGIN_URL, { email, password });
+            localStorage.setItem('jwt', response?.data?.token);
             setEmail('');
             setPassword('');
             navigate(from, { replace: true });
@@ -50,38 +41,58 @@ const Login = () => {
     }
 
     return (
-        <section>
-            <p ref={errorRef} className={errorMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errorMsg}</p>
-            <h1>Sign In</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">E-mail:</label>
-                <input
-                    type="text"
-                    id="email"
-                    ref={emailRef}
-                    autoComplete="off"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    required
-                />
-
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                />
-                <button>Sign In</button>
-            </form>
-            <p>
-                Need an Account?<br />
-                <span className="line">
-                    <Link to="/register">Sign Up</Link>
-                </span>
-            </p>
-        </section>
+        <>
+            <div className='main-login-container'>
+                <div className='login-container'>
+                    <div className='border-container'>
+                        <h1>Login </h1>
+                        <p
+                            ref={errorRef}
+                            className={errorMsg ? "errmsg" : "offscreen"}
+                            aria-live="assertive"
+                        >
+                            {errorMsg}
+                        </p>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group >
+                                <Form.Label htmlFor="email">email:</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="user@mail.com"
+                                    id="email"
+                                    ref={emailRef}
+                                    autoComplete="off"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                    required
+                                />
+                            </Form.Group>
+                            <Form.Group >
+                                <Form.Label htmlFor="password">password:</Form.Label>
+                                <Form.Control
+                                    placeholder="password"
+                                    type="password"
+                                    id="password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    value={password}
+                                    required
+                                />
+                            </Form.Group>
+                            <button className='btn-custom my-4'>
+                                <span className='btn-custom_top'> sign in
+                                </span>
+                            </button>
+                        </Form>
+                        <p>
+                            Need an Account?
+                        </p>
+                        <span className="line m-0">
+                            <Link to="/register">Sign Up</Link>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
