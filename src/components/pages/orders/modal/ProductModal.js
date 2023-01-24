a667a53178564a2c2dc2799ea55cb64df25ed732
thomas mafut <mafut50@gmail.com>
2023-01-24 11:57:32 -0300

    started modal changes

--- src/components/pages/orders/modal/ProductModal.js
+++ src/components/pages/orders/modal/ProductModal.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Accordion, Form, Modal } from 'react-bootstrap'
+import { Accordion, Form, Modal, Row, Col } from 'react-bootstrap'
 import Counter from '../../../counter/Counter';
+import './productmodal.css'
 
 const ProductModal = ({
     show,
@@ -102,9 +103,9 @@ const ProductModal = ({
             <Modal.Header closeButton>
                 <Modal.Title>{product.name}</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body className='p-0'>
                 <Form onSubmit={addToCart}>
-                    <Accordion>
+                    <Accordion className='w-100'>
                         <Accordion.Item eventKey="0">
                             <Accordion.Header>Remove ingredients</Accordion.Header>
                             <Accordion.Body>
@@ -147,42 +148,52 @@ const ProductModal = ({
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
-                    <h5>preferences</h5>
-                    <textarea className='textArea' name='preferences' id="mitextarea" rows="10" cols="50"></textarea>
-                    <div className="mb-3 d-flex justify-content-lg-around">
-                        <div>Size:</div>
-                        <Form.Check
-                            inline
-                            label={`medium (${defaultItem})`}
-                            name="size"
-                            type='radio'
-                            defaultChecked
-                            id='1'
-                            onChange={additionSize}
-                        />
-                        <Form.Check
-                            inline
-                            label={`large(${item2})`}
-                            name="size"
-                            type='radio'
-                            id='2'
-                            onChange={additionSize}
-                        />
-                    </div>
-                    <div className='button_container'>
-                        <Counter
-                            count={count}
-                            setCount={setCount} />
-                        <>$ {(product.price + addition) * count}</>
-                        <button className='icon-btn add-btn' type="submit">
-                            <div className="add-icon"></div>
-                            <div className="btn-txt">Add to Cart</div>
-                        </button>
+                    <div className="mb-3 p-3 d-flex flex-column justify-content-lg-around">
+                        <h5>preferences</h5>
+                        <textarea className='textArea' name='preferences' id="mitextarea" rows="10" cols="50"></textarea>
+                        <Row>
+                            <Col>
+                                <div className='size-container'>
+                                    <span>
+                                        Size:
+                                    </span>
+                                    <Form.Check
+                                        inline
+                                        label={`medium (${defaultItem})`}
+                                        name="size"
+                                        type='radio'
+                                        defaultChecked
+                                        id='1'
+                                        onChange={additionSize}
+                                    />
+                                    <Form.Check
+                                        inline
+                                        label={`large(${item2})`}
+                                        name="size"
+                                        type='radio'
+                                        id='2'
+                                        onChange={additionSize}
+                                    />
+                                </div>
+                            </Col>
+                            <Col>
+                                <div className='button_container d-flex flex-column justify-content-center align-content-center'>
+                                    <Counter
+                                        count={count}
+                                        setCount={setCount}
+                                    />
+                                    <span>$ {(product.price + addition) * count}</span>
+                                </div>
+                            </Col>
+                        </Row>
                     </div>
                 </Form>
             </Modal.Body>
-            <Modal.Footer>
-                footer
+            <Modal.Footer className='d-flex justify-content-center'>
+                <button className='icon-btn add-btn' type="submit">
+                    <div className="add-icon"></div>
+                    <div className="btn-txt">Add to Cart</div>
+                </button>
             </Modal.Footer>
         </Modal>
     )
