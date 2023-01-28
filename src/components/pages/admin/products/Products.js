b18049e56ccb98940fe51b13c37a2d55debbd61e
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2023-01-28 12:49:23 -0300

    added products page to abm page (#25)
    * progress in edit modal for products
    
    * showing ingredients list
    
    * started implementing form for add user
    
    * added edit modal to products
    
    * added edit funcionality to modal
    
    * finished products funcionality
    
    * added isVegan to products that are vegan

--- src/components/pages/admin/products/Products.js
+++ src/components/pages/admin/products/Products.js
@@ -1,25 +1,35 @@
-import React, { useState, useEffect } from 'react'
 import './products.css'
 import '../admin.css'
-import axios from 'axios';
-
-import { Table, Button, Alert, Accordion } from "react-bootstrap";
-
-import UserPlus from '../../../../assets/icons/light/UserPlus';
-import AccordionItem from 'react-bootstrap/esm/AccordionItem';
+import React, { useState, useEffect } from 'react'
+import axios from '../../../../api/axios';
+import { Table, Button, Alert, Accordion, Dropdown } from "react-bootstrap";
+import AddEditProductModal from './modal/AddEditProductModal';
+import Beer from '../../../../assets/icons/dark/Beer';
+import Leaf from '../../../../assets/icons/dark/Leaf';
 
 const Products = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [isEditing, setIsEditing] = useState(false)
     const [errorMessage, setErrorMessage] = useState('');
     const [productsToShow, setProductsToShow] = useState([]);
+    const [categoryToAdd, setCategoryToAdd] = useState('')
+    const [allCategories, setAllCategories] = useState([])
+
+    const [addEditModalShow, setAddEditModalShow] = useState(false);
+    const [productToEdit, setProductToEdit] = useState({});
+
+    const foodSizes = ['l', 'xl'];
+    const drinkSizes = ['750ml', '1,5lts'];
 
     useEffect(() => {
-        handleGetProoducts();
-    }, [])
+        getCategories();
+        handleGetProducts();
+        setIsLoading(false);
+    }, [addEditModalShow])
 
-
-    const handleGetProoducts = async () => {
+    const handleGetProducts = async () => {
         try {
-            const { data } = await axios.get('http://localhost:4000/api/product/');
+            const { data } = await axios.get('/product/');
             setProductsToShow(data?.products);
         } catch (error) {
             setErrorMessage(error.response.data.message);
@@ -27,20 +37,67 @@ const Products = () => {
         }
     };
 
+    const getCategories = async () => {
+        try {
+            const { data } = await axios.get('/category');
+            setAllCategories(data?.categories);
+        } catch (error) {
+            console.log('mori');
+        }
+    }
+
+    const handleDeleteProduct = async (categoryId, productId) => {
+        console.log(categoryId, productId);
+        try {
+            const { data } = await axios.patch(`/product/${categoryId.name}/delete/${productId}`, {});
+            console.log(data);
+            handleGetProducts();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleOpenEditModal = (product) => {
+        setProductToEdit(product);
+        setIsEditing(true);
+        setAddEditModalShow(true);
+    }
+
+    const handleOpenAddModal = (product, newProductCategory) => {
+        setProductToEdit(product);
+        setCategoryToAdd(newProductCategory);
+        setIsEditing(false);
+        setAddEditModalShow(true);
+    }
+
     return (
         <>
             <div className='abm-container'>
                 <div className="table-header">
                     <h1>Products Control Panel</h1>
-                    <Button variant='success'>
-                        <UserPlus />
-                    </Button>
                 </div>
                 {errorMessage ? (
                     <Alert variant="danger">{errorMessage}</Alert>
                 ) : (
                     ""
                 )}
+                <Dropdown className="m-1">
+                    <Dropdown.Toggle variant='danger' className="btn-dropdown">
+                        Add
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {
+                            allCategories?.map((category) => (
+                                <Dropdown.Item
+                                    onClick={() => handleOpenAddModal({}, category)}
+                                    className='w-100'
+                                >
+                                    {category?.name}
+                                </Dropdown.Item>
+                            ))
+                        }
+                    </Dropdown.Menu>
+                </Dropdown>
                 <Accordion>
                     {
                         productsToShow?.map((product, index) => (
@@ -49,24 +106,32 @@ const Products = () => {
                                     {product?.name}
                                 </Accordion.Header>
                                 <Accordion.Body>
-                                    <Table className='table-container' size='sm'>
-                                        <thead>
-                                            <tr>
-                                                <th className='col-3'>category</th>
-                                                <th className='col-3'>name</th>
-                                                <th className='col-3'>brand</th>
-                                                <th className='col-3'>price</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>{product?.category}</td>
-                                                <td>{product?.name}</td>
-                                                <td>{product?.brand}</td>
-                                                <td>{product?.price}</td>
-                                            </tr>
-                                        </tbody>
-                                    </Table>
+                                    <div className='overflow-table-container'>
+                                        <Table className='table-container' size='sm'>
+                                            <thead>
+                                                <tr>
+                                                    <th className='col-3'>category</th>
+                                                    <th className='col-3'>name</th>
+                                                    <th className='col-3'>price</th>
+                                                    {
+                                                        product?.category?.name === 'drink' &&
+                                                        <th>brand</th>
+                                                    }
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>{product?.category?.name}</td>
+                                                    <td>{product?.name}{product?.isVegan && <Leaf />}</td>
+                                                    <td>$ {product?.price}</td>
+                                                    {
+                                                        product?.category?.name === 'drink' &&
+                                                        <td>{product?.brand}{product?.hasAlcohol && <Beer />}</td>
+                                                    }
+                                                </tr>
+                                            </tbody>
+                                        </Table>
+                                    </div>
                                     {
                                         product?.ingredients &&
                                         <Accordion>
@@ -86,20 +151,49 @@ const Products = () => {
                                             </Accordion.Item>
                                         </Accordion>
                                     }
-                                    <div className='product-image-container p-3 my-3'>
-                                        <img src={product?.image} alt={'image of' + product?.name} className='product-image' />
-                                        <Button className='product-image-button d-flex mt-3'>
-                                            <span>
-                                                Change Image
-                                            </span>
-                                        </Button>
+                                    <div className='w-100 d-flex justify-content-evenly'>
+                                        <div className='product-image-container p-3 my-3'>
+                                            <span className='mb-2'>Image: </span>
+                                            <img src={product?.image} alt={'image of' + product?.name} className='product-image' />
+                                        </div>
+                                        <div className='product-size-container p-3 my-3'>
+                                            <span>Sizes available: </span>
+                                            {
+                                                product?.category?.name === 'drink' ?
+                                                    <ul>
+                                                        {
+                                                            drinkSizes.map((size) => (
+                                                                <li>{size}</li>
+                                                            ))
+                                                        }
+                                                    </ul>
+                                                    :
+                                                    <ul>
+                                                        {
+                                                            foodSizes.map((size) => (
+                                                                <li>{size}</li>
+                                                            ))
+                                                        }
+                                                    </ul>
+                                            }
+                                        </div>
                                     </div>
+                                    <Button onClick={() => handleOpenEditModal(product)}>Edit</Button>
+                                    <Button variant='danger' onClick={() => handleDeleteProduct(product?.category, product?._id)}>Delete</Button>
                                 </Accordion.Body>
                             </Accordion.Item>
                         ))
                     }
                 </Accordion>
             </div>
+            <AddEditProductModal
+                show={addEditModalShow}
+                setShow={setAddEditModalShow}
+                product={productToEdit}
+                setProduct={setProductToEdit}
+                categoryToAdd={categoryToAdd}
+                isEditing={isEditing}
+            />
         </>
     )
 }
