6d8c42b98616c3a24ff8dae726fb8f61951cebb1
thomas mafut <mafut50@gmail.com>
2023-01-28 16:31:05 -0300

    moved icons to one folder n changed colors to black

--- src/assets/icons/Edit.js
+++ src/assets/icons/Edit.js
@@ -5,15 +5,15 @@ const Edit = () => {
         <>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="icon icon-tabler icon-tabler-pencil"
+                className="icon icon-tabler icon-tabler-pencil"
                 width="44"
                 height="44"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="#ffffff"
+                strokeWidth="1.5"
+                stroke="#000000"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
