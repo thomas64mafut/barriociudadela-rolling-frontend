3f37e5f8593bb0a88fcf0f7fa1776c95af0e9bca
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2023-01-28 13:24:47 -0300

    Header fix (#28)
    * nuevo navbar prueba
    
    * nuevos cambios en navbar
    
    * fixed navbar position and transparent background
    
    * changes on responsive navbar
    
    * changes on navbar height
    
    * idk why is this here
    
    ---------
    
    Co-authored-by: Tomas Garcia Cardenas <tomass11052001@hotmail.com>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,38 @@
-import React from 'react'
 import './header.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/img/logo-red.png'
 
 const Header = () => {
   return (
-    <div>
-      <div className='navHome d-flex justify-content-around align-items-center'>
-        <div>
-          <a className='navOptions boton1'>ABOUT US</a>
-          <a className='navOptions boton1'>MENUS</a>
-          <a className='navOptions boton1'>FOLLOW US</a>
-        </div>
-        <div>
-          <img className='logo' src={logo} alt=''/>
-        </div>
-        <div>
-          <a className='navOptions boton1'>CONTACT US</a>
-          <a className='navOptions boton1'>LOGIN</a>
-          <a className='navOptions boton1'>REGISTER</a>
-        </div>
-      </div>
-    </div>
-  )
+    <Navbar className='navContainer' expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#"> <img className='logoa' src={logo}/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0 locura"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <div className='primerNav'>
+              <div href="#action1" className='navOptions boton1'>ABOUT US</div>
+              <div href="#action2" className='navOptions boton1'>MENU</div>
+              <div href="#" className='navOptions boton1' >
+                FOLLOW US
+              </div>
+            </div>
+          </Nav>
+          <div className='segundoNav'>
+            <div className='navOptions boton1'>CONTACT US</div>
+            <div className='navOptions boton1'>LOGIN</div>
+            <div className='navOptions boton1'>REGISTER</div>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
