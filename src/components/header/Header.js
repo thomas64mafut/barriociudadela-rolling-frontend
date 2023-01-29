1c700fa6eb9b31c0ec12c54a9651500f73188834
Tomas Garcia Cardenas <tomass11052001@hotmail.com>
2023-01-29 12:29:28 -0300

    fixed navbar and linked to other directions

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <Navbar className='navContainer' expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#"> <img className='logoa' src={logo}/></Navbar.Brand>
+        <Navbar.Brand href="#"> <a onClick={() => window.location.href = 'http://localhost:3000/'}><img className='logoa' src={logo}/></a> </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -17,17 +17,17 @@ const Header = () => {
             navbarScroll
           >
             <div className='primerNav'>
-              <div href="#action1" className='navOptions boton1'>ABOUT US</div>
-              <div href="#action2" className='navOptions boton1'>MENU</div>
-              <div href="#" className='navOptions boton1' >
-                FOLLOW US
+              <div href="#action1" className='navOptions boton1 fourOne'> <a className='follow' href='aboutUs'>ABOUT US</a></div>
+              <div href="#action2" className='navOptions boton1' onClick={() => window.location.href = 'http://localhost:3000/menus'}>MENU</div>
+              <div className='navOptions boton1 fourOne' >
+                <a className='follow' href="#followUs">FOLLOW US</a>
               </div>
             </div>
           </Nav>
           <div className='segundoNav'>
-            <div className='navOptions boton1'>CONTACT US</div>
-            <div className='navOptions boton1'>LOGIN</div>
-            <div className='navOptions boton1'>REGISTER</div>
+            <a href='https://wa.me/543816681643' target="_blank" className='navOptions boton1'>CONTACT US</a>
+            <div className='navOptions boton1'> <a onClick={() => window.location.href = 'http://localhost:3000/login'}>LOGIN</a> </div>
+            <div className='navOptions boton1'> <a onClick={() => window.location.href = 'http://localhost:3000/register'}>REGISTER</a> </div>
           </div>
         </Navbar.Collapse>
       </Container>
