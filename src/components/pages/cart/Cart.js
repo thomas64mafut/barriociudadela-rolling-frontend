5b662a6c3d5891daf7ea19417fb8ad6715cd3a74
thomas mafut <mafut50@gmail.com>
2023-01-31 21:47:25 -0300

    basic styling in carts

--- src/components/pages/cart/Cart.js
+++ src/components/pages/cart/Cart.js
@@ -1,92 +1,92 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Spinner, Table } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import axios from '../../../api/axios';
-import BuyModal from './cart modal/BuyModal';
+
 import './cart.css'
 
 
-export const Cart = ({cart}) => {
+export const Cart = ({ cart }) => {
     let navigate = useNavigate();
-    const [buyModalShow, setBuyModalShow] = useState(false)
+
     const [cartTotalPrice, setCartTotalPrice] = useState(0)
-    let idkey=0;
-    let totalPrice=0;
+    let idkey = 0;
+    let totalPrice = 0;
 
     useEffect(() => {
-        totalPrice=0;
+        totalPrice = 0;
         for (const product of cart?.products) {
-            totalPrice = totalPrice+(product?.price*product?.quantity)
+            totalPrice = totalPrice + (product?.price * product?.quantity)
         }
         setCartTotalPrice(totalPrice)
     }, [])
-    
-    const handleDeleteCart = async() => {
-        try {
-            const { data } = await axios.patch('/cart/'+cart._id)
-            navigate('/menus')
-        } catch (error) {
-            console.log(error)
-        }
-    }
 
-  return (
-    <div className={cart.cartStatus}>
-        <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                    <th>#</th>
-                                    <th>Product Name</th>
-                                    <th>Preferences</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
+    return (
+        <div className={cart.cartStatus + ' w-100'}>
+            <Table bordered className='w-100 cart-table-container'>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>product name</th>
+                        <th>preferences</th>
+                        <th>quantity</th>
+                        <th>price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        cart?.products?.map((product) => {
+                            idkey = idkey + 1;
+                            return (
+                                <tr key={idkey}>
+                                    <td>{idkey}</td>
+                                    <td>{product.name}</td>
+                                    <td>
+                                        size: {product.size}
+                                        {
+                                            product.removed.length > 0 &&
+                                            <tr>
+                                                to remove: {
+                                                    product.removed.map((i, index) => {
+                                                        if (product?.removed.length === index + 1) {
+                                                            return `${i.name}`
+                                                        } else return `${i.name}, `
+                                                    })
+                                                }
+                                            </tr>
+                                        }
+                                        {
+                                            product.toppings.length > 0 &&
+                                            <tr>
+                                                to topping: {
+                                                    product.toppings.map((i, index) => {
+                                                        if (product?.toppings.length === index + 1) {
+                                                            return `${i.name}`
+                                                        } else return `${i.name}, `
+                                                    })
+                                                }
+                                            </tr>
+                                        }
+                                        {
+                                            product.preferences &&
+                                            <tr>
+                                                preferences: {product.preferences}
+                                            </tr>
+                                        }
+                                    </td>
+                                    <td>{product.quantity}</td>
+                                    <td>{product.quantity * product.price}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td className='total-price'>total</td>
+                    <td className='total-price'> $ {cartTotalPrice}</td>
+                </tbody>
+            </Table>
 
-                                    {    
-                                        cart?.products?.map(product => {
-                                            idkey= idkey+1;
-                                            return(
-                                                <tr key={idkey}>
-                                                    <td>{idkey}</td>
-                                                    <td>{product.name}</td>
-                                                    <td>size: {product.size} 
-                                                        {product.removed.length > 0 && <tr>to remove: {product.removed.map((i) => {return `-${i.name}`})}</tr>}
-                                                        {product.toppings.length > 0 && <tr>to topping: {product.toppings.map((i) => {return `-${i.name}`})}</tr>}
-                                                        {product.preferences && <tr>preferences: {product.preferences}</tr>}
-                                                    </td>
-                                                    <td>{product.quantity}</td>
-                                                    <td>{product.quantity*product.price}</td>
-                                                </tr>
-                                                )
-                                            })
-                                    }
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>Total</td>
-                                <td> $ {cartTotalPrice}</td>
-                                </tbody>
-                            </Table>
-                            <div>
-                                {
-                                    cart.cartStatus==='active'? (
-                                        <div>
-                                            <Button variant='secondary' onClick={handleDeleteCart}>Delete Cart</Button>
-                                            <Button variant='secondary' onClick={()=>setBuyModalShow(true)}>Buy now</Button>
-                                        </div>
-                                    ) : (
-                                        <></>
-                                    )
-                                    
-                                }
-                            </div>
-                            <BuyModal
-                                show={buyModalShow}
-                                setShow={setBuyModalShow}
-                                cart={cart}
-                            />
-    </div>
-  )
+        </div>
+    )
 }
--- src/components/pages/cart/Carts.js
+++ src/components/pages/cart/Carts.js
@@ -1,83 +1,144 @@
 import axios from '../../../api/axios';
 import React, { useEffect, useState } from 'react'
-import {Accordion, Button, Spinner, Table} from 'react-bootstrap';
+import { Accordion, Button, Spinner } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import BuyModal from './cart modal/BuyModal';
 import { Cart } from './Cart';
-import './cart.css'
+import './cart.css';
+import BuyModal from './cart modal/BuyModal';
 
 const Carts = () => {
     let navigate = useNavigate();
     const [carts, setCarts] = useState({})
-    const [activeCart, setActiveCart] = useState({})   
-    
-    let idkey=0
-    
+    const [activeCart, setActiveCart] = useState({})
+    const [buyModalShow, setBuyModalShow] = useState(false)
+
     useEffect(() => {
         handleGetCart();
     }, [])
 
-    const handleGetCart =  async () => {
+    const handleGetCart = async () => {
         try {
             const { data } = await axios('/cart');
             const activeCart = data.ownCarts.find(cart => cart.cartStatus === 'active')
-            console.log(activeCart)
-            const cartstoShow = data.ownCarts.filter(cart => cart.cartStatus === 'bought' || cart.cartStatus === 'cancelled' || cart.cartStatus === 'delivered' || cart.cartStatus === 'preparing' )
+            const cartstoShow = data.ownCarts.filter(cart => cart.cartStatus === 'bought' || cart.cartStatus === 'cancelled' || cart.cartStatus === 'delivered' || cart.cartStatus === 'preparing')
             setActiveCart(activeCart)
             setCarts(cartstoShow.reverse())
         } catch (error) {
             console.log(error)
         }
     }
-    
-    
+
+    const handleDeleteCart = async () => {
+        try {
+            const { data } = await axios.patch('/cart/' + activeCart._id)
+            navigate('/menus')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div>
+        <div className='main-container'>
             {
-                activeCart?._id? (
-                    <>
-                                    <h3>Your active Cart</h3>
-                                    <div>Cart_id: {activeCart._id}</div>
-                                    <div>Status: {activeCart.cartStatus}</div>
-                                    <Cart
-                                    cart = {activeCart}
-                                    />
-                    
-                    </>
-                ):(
-                    <h4>You don't have active Cart</h4>
+                activeCart?._id ? (
+                    <div>
+                        <h3>Your active Cart</h3>
+                        <div className='p-3 active-cart-container'>
+                            <div className='ps-2'>
+                                <div>Cart_id: {activeCart._id}</div>
+                                <div className='mb-3'>Status: {activeCart.cartStatus}</div>
+                            </div>
+                            <div className='overflow-table-container'>
+                                <Cart
+                                    cart={activeCart}
+                                />
+                            </div>
+                        </div>
+                        <div className='w-100'>
+                            {
+                                activeCart.cartStatus === 'active' &&
+                                <div className='w-100 mb-3 button-buy-cart-container flex-column flex-sm-row'>
+                                    <Button
+                                        variant='danger'
+                                        onClick={handleDeleteCart}
+                                    >
+                                        Delete Cart
+                                    </Button>
+                                    <Button
+                                        variant='success'
+                                        onClick={() => setBuyModalShow(true)}
+                                    >
+                                        Buy now
+                                    </Button>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                ) : (
+                    <h2>You don't have active Cart</h2>
                 )
             }
-            <Accordion defaultActiveKey="0">
-                    <Accordion.Header>Your last Carts</Accordion.Header>
-                    <Accordion.Body>
-                        {
-                            carts?.length? (
-                                carts?.map((cart) => {
-                                        return (
-                                            <>
-                                                <div>Cart_id: {cart?._id}</div>
-                                                <div>Status: {cart?.cartStatus}</div>
-                                                {
-                                                    cart?.cartStatus === 'bought'? (<div>Bought on {new Date(cart?.boughtAt).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</div>):(<></>)
-                                                }
-                                                <Cart
-                                                cart = {cart}
-                                                />
-                                            </>
-                                        )
-                                }
+            <Accordion defaultActiveKey="0" >
+                <Accordion.Header className='orders-title'>your last carts</Accordion.Header>
+                <Accordion.Body>
+                    {
+                        carts?.length ? (
+                            carts?.map((cart) => {
+                                return (
+                                    <div className={'order-container ' + cart?.cartStatus}>
+                                        <div className='w-100 py-2'>
+                                            <div className='w-100'>
+                                                <div className='ps-3'>
+                                                    <div>Cart id: {cart?._id}</div>
+                                                    <div className='mb-2'>Status: {cart?.cartStatus}</div>
+                                                    {
+                                                        cart?.cartStatus === 'bought' &&
+                                                        <div>
+                                                            Bought on {
+                                                                new Date(cart?.boughtAt).toLocaleDateString('en-us', {
+                                                                    weekday: "long",
+                                                                    year: "numeric",
+                                                                    month: "short",
+                                                                    day: "numeric"
+                                                                })
+                                                            }
+                                                        </div>
+                                                    }
+                                                </div>
+                                                <div className='overflow-table-container'>
+                                                    <Cart
+                                                        cart={cart}
+                                                        className='w-100'
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 )
-                            ):
+                            }
+                            )
+                        ) :
                             (
                                 <Spinner className='spinnerLoading' animation="border" variant="success" />
                             )
-                        }
-                    </Accordion.Body>
+                    }
+                </Accordion.Body>
             </Accordion>
-          
-            
-        <Button variant='secondary' onClick={() => navigate('/menus')}>Back to menu</Button>
+
+            <div className='w-100 text-center mb-3 button-buy-cart-container'>
+                <Button
+                    variant='secondary'
+                    onClick={() => navigate('/menus')}
+                >
+                    Back to menu
+                </Button>
+            </div>
+
+            <BuyModal
+                show={buyModalShow}
+                setShow={setBuyModalShow}
+                cart={activeCart}
+            />
         </div>
     )
 }
