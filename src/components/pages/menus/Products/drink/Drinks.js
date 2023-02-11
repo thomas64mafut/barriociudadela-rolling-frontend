46487b93bbc5649f657ba881114b455c31fc36f0
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2023-02-11 12:19:05 -0300

    adjustments in cards (#72)
    * added adjustments in cards
    
    * fixed login animation in cards

--- src/components/pages/menus/Products/drink/Drinks.js
+++ src/components/pages/menus/Products/drink/Drinks.js
@@ -1,21 +1,28 @@
-import './drinks.css'
-import React, { useEffect, useState } from 'react'
-import { Spinner } from 'react-bootstrap'
+import './drinks.css';
+import React, { useEffect, useState } from 'react';
 import axios from '../../../../../api/axios';
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap';
 
 import DrinksCard from './DrinksCard';
+import Loading from '../../../../loading/Loading';
 
 
-const Drinks = ({ category, defaultItem, item2, setError, setMessageModalShow, setMessageToShow }) => {
+const Drinks = ({ defaultItem, item2, setError, setMessageModalShow, setMessageToShow }) => {
     const [drinks, setDrinks] = useState();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        handleGetdrinks();
+        handleGetData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const handleGetdrinks = async () => {
+    const handleGetData = async () => {
+        setIsLoading(true);
+        await handleGetDrinks();
+        setIsLoading(false);
+    }
+
+    const handleGetDrinks = async () => {
         try {
             const { data } = await axios('/product/drink')
             setDrinks(data.Drinks)
@@ -26,31 +33,44 @@ const Drinks = ({ category, defaultItem, item2, setError, setMessageModalShow, s
 
     return (
         <div>
-            <div className='cards_container'>
-                <Row className='p-3 drink-container'>
-                    {
-                        drinks?.length ? (
-                            drinks?.map((drink, index) => {
-                                return (
-                                    <Col md={6} lg={4} className='d-flex justify-content-center mb-4' key={index}>
-                                        <DrinksCard
-                                            key={drink._id}
-                                            drink={drink}
-                                            defaultItem={defaultItem}
-                                            item2={item2}
-                                            setError={setError}
-                                            setMessageToShow={setMessageToShow}
-                                            setMessageModalShow={setMessageModalShow}
-                                        />
-                                    </Col>
-                                ) 
-                            })
-                        ) : (
-                            <Spinner className='spinnerLoading' animation="border" variant="success" />
-                        )
-                    }
-                </Row>
-            </div>
+            {
+                isLoading
+                    ? (
+                        <Loading />
+                    )
+                    : (
+                        <div className='cards_container'>
+                            <Row className='p-3 drink-container'>
+                                {
+                                    drinks?.length ? (
+                                        drinks?.map((drink, index) => {
+                                            return (
+                                                <Col
+                                                    md={6}
+                                                    lg={4}
+                                                    className='d-flex justify-content-center mb-4'
+                                                    key={index}
+                                                >
+                                                    <DrinksCard
+                                                        key={drink._id}
+                                                        drink={drink}
+                                                        defaultItem={defaultItem}
+                                                        item2={item2}
+                                                        setError={setError}
+                                                        setMessageToShow={setMessageToShow}
+                                                        setMessageModalShow={setMessageModalShow}
+                                                    />
+                                                </Col>
+                                            )
+                                        })
+                                    ) : (
+                                        <h4 className='w-100 text-center'>there are no products in this category</h4>
+                                    )
+                                }
+                            </Row>
+                        </div>
+                    )
+            }
         </div>
     )
 }
