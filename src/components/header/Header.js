aed50516fdc08c65e68a7fd6424edb8bbd0d9be2
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2023-02-28 16:33:25 -0300

    fixed blur in header (#105)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,15 @@
 import './header.css';
 import React, { useState, useEffect, useContext } from 'react';
-import { Navbar, Tooltip, OverlayTrigger, InputGroup, Button } from 'react-bootstrap';
+import {
+  Navbar,
+  Tooltip,
+  OverlayTrigger,
+  InputGroup,
+  Button,
+} from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { ThemeContext } from '../../context/ThemeContext';
 import ThemeToggler from '../themeToggler/ThemeToggler';
-
 
 import logo from '../../assets/img/logo-red.png';
 import axios from '../../api/axios';
@@ -20,6 +25,8 @@ const Header = () => {
   const [cartTotalPrice, setCartTotalPrice] = useState('');
   const [profileImg, setProfileImg] = useState('');
   const [setErrorMessage] = useState('');
+  const [mouseActive, setMouseActive] = useState(false);
+
   const { darkMode } = useContext(ThemeContext);
 
   useEffect(() => {
@@ -89,9 +96,11 @@ const Header = () => {
     try {
       const { data } = await axios('/cart');
       let totalPrice = 0;
-      const activeCart = data?.ownCarts?.find(cart => cart.cartStatus === 'active');
+      const activeCart = data?.ownCarts?.find(
+        (cart) => cart.cartStatus === 'active'
+      );
       for (const product of activeCart.products) {
-        totalPrice = totalPrice + (product?.price * product?.quantity);
+        totalPrice = totalPrice + product?.price * product?.quantity;
       }
       setCartTotalPrice(totalPrice);
     } catch (error) {
@@ -101,106 +110,121 @@ const Header = () => {
 
   const cartPriceTooltip = (
     <Tooltip id="cart-price">
-      <span className='cart-price'>total: ${cartTotalPrice}</span>
+      <span className="cart-price">total: ${cartTotalPrice}</span>
     </Tooltip>
   );
 
   return (
     <Navbar
-      className={darkMode
-        ? 'navContainer-dark m-0'
-        : 'navContainer m-0'
-      }
+      className={darkMode ? 'navContainer-dark m-0' : 'navContainer m-0'}
       expand="lg"
       expanded={expanded}
-      onBlur={() => setExpanded(false)}
+      onBlur={() => {
+        if (!mouseActive) {
+          setExpanded(false);
+        } else {
+          setExpanded(expanded);
+        }
+      }}
+      onMouseEnter={() => setMouseActive(true)}
+      onMouseLeave={() => setMouseActive(false)}
     >
       <Navbar.Brand href="/">
-        <img className='logoa' src={logo} alt='' />
+        <img className="logoa" src={logo} alt="" />
       </Navbar.Brand>
       <Navbar.Toggle
         aria-controls="navbarScroll"
-        className='me-2 p-0 border-0'
+        className="me-2 p-0 border-0"
         onClick={() => setExpanded(expanded ? false : 'expanded')}
       />
       <Navbar.Collapse id="navbarScroll">
-        <div className="w-100 h-100 d-flex flex-lg-row flex-column justify-content-between navbar-complete" >
-          <div className='d-flex flex-lg-row flex-column nav-options-container'>
-            <Link to={'/'} className='navOptions boton1' onClick={() => setExpanded(false)}>
-                            ABOUT US
+        <div className="w-100 h-100 d-flex flex-lg-row flex-column justify-content-between navbar-complete">
+          <div className="d-flex flex-lg-row flex-column nav-options-container">
+            <Link
+              to={'/'}
+              className="navOptions boton1"
+              onClick={() => setExpanded(false)}
+            >
+              ABOUT US
             </Link>
-            <Link href='https://wa.me/543816681643' className='navOptions boton1' onClick={() => setExpanded(false)}>
-                            CONTACT US
+            <Link
+              href="https://wa.me/543816681643"
+              className="navOptions boton1"
+              onClick={() => setExpanded(false)}
+            >
+              CONTACT US
             </Link>
-            <Link to={'/menus'} className='navOptions boton1' onClick={() => setExpanded(false)}>
-                            MENU
+            <Link
+              to={'/menus'}
+              className="navOptions boton1"
+              onClick={() => setExpanded(false)}
+            >
+              MENU
             </Link>
             <ThemeToggler />
           </div>
-          <div className='d-flex flex-lg-row flex-column '>
-            {
-              token
-                ? (
-                  <div className='d-flex flex-lg-row flex-column nav-options-container me-lg-1'>
-                    {
-                      userRole === 'admin' &&
-                                            <>
-                                              <Link to={'/orders'} className='navOptions boton1' onClick={() => setExpanded(false)}>
-                                                    orders
-                                              </Link>
-                                            </>
-                    }
-                    <InputGroup className='profile-panel'>
-                      {
-                        userRole !== 'admin'
-                          ? (
-                            <OverlayTrigger
-                              placement="bottom"
-                              delay={{ show: 250, hide: 400 }}
-                              overlay={cartPriceTooltip}
-                            >
-                              <Button onClick={showCart} className='p-0 cart-button'>
-                                <Cart />
-                              </Button>
-                            </OverlayTrigger>
-                          )
-                          : (
-                            <Button className='p-0 cart-button' onClick={showAdminPage}>
-                              <Config />
-                            </Button>
-                          )
+          <div className="d-flex flex-lg-row flex-column ">
+            {token ? (
+              <div className="d-flex flex-lg-row flex-column nav-options-container me-lg-1">
+                {userRole === 'admin' && (
+                  <>
+                    <Link
+                      to={'/orders'}
+                      className="navOptions boton1"
+                      onClick={() => setExpanded(false)}
+                    >
+                      orders
+                    </Link>
+                  </>
+                )}
+                <InputGroup className="profile-panel">
+                  {userRole !== 'admin' ? (
+                    <OverlayTrigger
+                      placement="bottom"
+                      delay={{ show: 250, hide: 400 }}
+                      overlay={cartPriceTooltip}
+                    >
+                      <Button onClick={showCart} className="p-0 cart-button">
+                        <Cart />
+                      </Button>
+                    </OverlayTrigger>
+                  ) : (
+                    <Button className="p-0 cart-button" onClick={showAdminPage}>
+                      <Config />
+                    </Button>
+                  )}
+                  <InputGroup.Text className="">
+                    <div className="d-flex flex-column align-items-end">
+                      <span>Welcome {userToShow?.username}!</span>
+                      <button onClick={handleLogOut} className="border-0">
+                        log out
+                      </button>
+                    </div>
+                  </InputGroup.Text>
+                  <InputGroup.Text
+                    className="img-profile-container"
+                    onClick={showUserProfile}
+                  >
+                    <img
+                      className={
+                        darkMode ? 'img-profile-pic-dark' : 'img-profile-pic'
                       }
-                      <InputGroup.Text className=''>
-                        <div className='d-flex flex-column align-items-end'>
-                          <span>
-                                                        Welcome {userToShow?.username}!
-                          </span>
-                          <button onClick={handleLogOut} className='border-0'>log out</button>
-                        </div>
-                      </InputGroup.Text>
-                      <InputGroup.Text
-                        className="img-profile-container"
-                        onClick={showUserProfile}
-                      >
-                        <img
-                          className={darkMode ? 'img-profile-pic-dark' : 'img-profile-pic'}
-                          src={profileImg} alt=""
-                        />
-                      </InputGroup.Text>
-                    </InputGroup>
-                  </div>
-                )
-                : (
-                  <div className='d-flex flex-lg-row flex-column nav-options-container me-3'>
-                    <Link to={'/login'} className='navOptions boton1'>
-                                            LOGIN
-                    </Link>
-                    <Link to={'/register'} className='navOptions boton1'>
-                                            REGISTER
-                    </Link>
-                  </div>
-                )
-            }
+                      src={profileImg}
+                      alt=""
+                    />
+                  </InputGroup.Text>
+                </InputGroup>
+              </div>
+            ) : (
+              <div className="d-flex flex-lg-row flex-column nav-options-container me-3">
+                <Link to={'/login'} className="navOptions boton1">
+                  LOGIN
+                </Link>
+                <Link to={'/register'} className="navOptions boton1">
+                  REGISTER
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </Navbar.Collapse>
